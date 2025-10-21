f3af64e1e66063c2bd7db8d40416da7b56b66930
richardfreiree <richardzzfreirezz@gmail.com>
2025-10-21 19:16:40 -0300

    sobe sobe

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Formulário de cadastro</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
+
+	<h1>Cadastro de Usuários</h1>
 
 
 </body>
