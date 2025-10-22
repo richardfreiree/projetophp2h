44d7078f07dd5df5a3cc081af3f2d59ad1a980cb
richardfreiree <richardzzfreirezz@gmail.com>
2025-10-21 21:56:18 -0300

    sobe sobe

--- include/cadastrar.php
+++ include/cadastrar.php
@@ -4,4 +4,9 @@ $sobrenome = $_POST['sobrenome'];
 $escolhaPets = $_POST['escolhaPets'];
 
 echo($primeiroNome . " - " . $sobrenome . " - " . $escolhaPets);
+
+echo '<br><br><a href="../index.php">Voltar</a>';
+
+// header("location: ../index.php");
+
 ?>
